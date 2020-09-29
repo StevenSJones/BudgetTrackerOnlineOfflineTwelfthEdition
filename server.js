42b28cc22d6d28ec79679228d2857c32ea945606
SS Jones <SSJones.code@gmail.com>
2020-09-29 16:36:46 -0700

    I included the script tag in the index.html file and the service slash worker.js file in the public dir. After running npm start in int, going to localhost colon 3000 in browser, dev tools, application and then service workers, i see that they are activated and running.

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
+//require our npm packages
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+//set our port to 3000
 const PORT = 3000;
-
+//creating the express app
 const app = express();
-
+//using the morgan logger middleware
 app.use(logger("dev"));
-
+//using compression middleware
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+//middlware serving up our static files
 app.use(express.static("public"));
-
+//connecting to mongoose
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
@@ -22,7 +23,7 @@ mongoose.connect("mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
-
+//set our port to listen
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
