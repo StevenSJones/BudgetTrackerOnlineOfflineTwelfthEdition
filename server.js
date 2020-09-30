f0a9dddd5d1865ccd04accff756d38c2b45a4b30
SS Jones <SSJones.code@gmail.com>
2020-09-29 18:54:34 -0700

    I added the unified topology that was recommended in the terminal when I run node server.js. I also added to the service worker file what we did in the activities in class and trimmed it a bit. It shows my cache name in the application tab in dev tools but not the actual tabular data that Tucker got in class.

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(express.static("public"));
 //connecting to mongoose
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true 
 });
 
 // routes
