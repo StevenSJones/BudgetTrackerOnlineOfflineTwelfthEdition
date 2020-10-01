a39107e28f000244b0c4329f46d6927dece84203
SS Jones <SSJones.code@gmail.com>
2020-10-01 07:49:43 -0700

    I was able to get the favicon to work by adding a new image that i modified via a favicon website. I then erased the old image and inserted this one, being sure to name it what the last image was. I also ammended the size in the manifest and I made the app connect on eighty eighty with the env line that aids in connecting to heroku.

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 //set our port to 3000
-const PORT = 3000;
+// const PORT = 3000;
+//set the port of the server allowing heroku
+const PORT = process.env.PORT || 8080;
 //creating the express app
 const app = express();
 //using the morgan logger middleware
@@ -15,12 +17,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //middlware serving up our static files
 app.use(express.static("public"));
+
+// const uri = "mongodb://localhost/budget";
+
+const uri = "mongodb+srv://Steven:Password1@cluster0.s4hbt.mongodb.net/budget?retryWrites=true&w=majority";
 //connecting to mongoose
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true 
-});
+}).then(() => {
+  console.log("Connected!")
+})
+.catch(err => console.log(err))
 
 // routes
 app.use(require("./routes/api.js"));
