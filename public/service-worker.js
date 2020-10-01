627e6ac85ca29e5971bd0dfa34083dbd009abf58
SS Jones <SSJones.code@gmail.com>
2020-09-30 18:43:40 -0700

    I inlcuded the remainder of the code to service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,72 +12,64 @@ const FILES_TO_CACHE = [
   "/assets/images/icons/icon-192x192.png",
   "/assets/images/icons/icon-512x512.png",
 ];
-install;
-self.addEventListener("install", function (evt) {
-  // pre cache image data
-  evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-  );
 
-  // pre cache all static assets
+self.addEventListener("install", function(evt) {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+      caches.open(CACHE_NAME).then(cache => {
+          console.log("Your files were pre-cached successfully!");
+          return cache.addAll(FILES_TO_CACHE);
+      })
   );
-
-  // tell the browser to activate this service worker immediately once it has finished installing
   self.skipWaiting();
 });
-
-// activate
-self.addEventListener("activate", function (evt) {
+self.addEventListener("activate", function(evt) {
   evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+      caches.keys().then(keyList => {
+          return Promise.all(
+              keyList.map(key => {
+                  if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                      console.log("Removing old cache data", key);
+                      return caches.delete(key);
+                  }
+              })
+          );
+      })
   );
-
   self.clients.claim();
 });
-
 // fetch
-self.addEventListener("fetch", function (evt) {
+self.addEventListener("fetch", function(evt) {
+  // cache successful requests to the API
   if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
-          return fetch(evt.request)
-            .then((response) => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-
-              return response;
-            })
-            .catch((err) => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        })
-        .catch((err) => console.log(err))
-    );
-
-    return;
+      evt.respondWith(
+          caches.open(DATA_CACHE_NAME).then(cache => {
+              return fetch(evt.request)
+                  .then(response => {
+                      // If the response was good, clone it and store it in the cache.
+                      if (response.status === 200) {
+                          cache.put(evt.request.url, response.clone());
+                      }
+                      return response;
+                  })
+                  .catch(err => {
+                      // Network request failed, try to get it from the cache.
+                      return cache.match(evt.request);
+                  });
+          }).catch(err => console.log(err))
+      );
+      return;
   }
-
+  // if the request is not for the API, serve static assets using "offline-first" approach.
+  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
   evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
-      });
-    })
-  );
+      fetch(evt.request).catch(function() {
+          return caches.match(evt.request).then(function(response) {
+              if (response) {
+                  return response;
+              } else if (evt.request.headers.get("accept").includes("text/html")) {
+                  return caches.match("/");
+              }
+          });
+      })
+  )
 });
