ea9f9292c5da8c4b7f73701718e9a77b4da0b22a
SS Jones <SSJones.code@gmail.com>
2020-09-30 18:46:00 -0700

    I added the formatting and the commentary explaining to me the error.

--- public/db.js
+++ public/db.js
@@ -2,13 +2,13 @@ let db;
 // create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
 //in the event that an update is required
-request.onupgradeneeded = function(event) {
-   // create object store called "pending" and set autoIncrement to true
+request.onupgradeneeded = function (event) {
+  // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
 //in the event of success
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
   db = event.target.result;
 
   // check if app is online before reading from db
@@ -17,7 +17,7 @@ request.onsuccess = function(event) {
   }
 };
 //in the event of an error
-request.onerror = function(event) {
+request.onerror = function (event) {
   console.log("Woops! " + event.target.errorCode);
 };
 
@@ -40,27 +40,27 @@ function checkDatabase() {
   // get all records from store and set to a variable
   const getAll = store.getAll();
 
-  getAll.onsuccess = function() {
+  getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
       fetch("/api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
           Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
+          "Content-Type": "application/json",
+        },
       })
-      .then(response => response.json())
-      .then(() => {
-        // if successful, open a transaction on your pending db
-        const transaction = db.transaction(["pending"], "readwrite");
+        .then((response) => response.json())
+        .then(() => {
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["pending"], "readwrite");
 
-        // access your pending object store
-        const store = transaction.objectStore("pending");
+          // access your pending object store
+          const store = transaction.objectStore("pending");
 
-        // clear all items in your store
-        store.clear();
-      });
+          // clear all items in your store
+          store.clear();
+        });
     }
   };
 }
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+//watch the file path. You can always right click and select relative file path.
 //the files that we will cache
 const FILES_TO_CACHE = [
   "/",
@@ -13,63 +14,66 @@ const FILES_TO_CACHE = [
   "/assets/images/icons/icon-512x512.png",
 ];
 
-self.addEventListener("install", function(evt) {
+self.addEventListener("install", function (evt) {
   evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-          console.log("Your files were pre-cached successfully!");
-          return cache.addAll(FILES_TO_CACHE);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
-self.addEventListener("activate", function(evt) {
+self.addEventListener("activate", function (evt) {
   evt.waitUntil(
-      caches.keys().then(keyList => {
-          return Promise.all(
-              keyList.map(key => {
-                  if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                      console.log("Removing old cache data", key);
-                      return caches.delete(key);
-                  }
-              })
-          );
-      })
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
   self.clients.claim();
 });
 // fetch
-self.addEventListener("fetch", function(evt) {
+self.addEventListener("fetch", function (evt) {
   // cache successful requests to the API
   if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-          caches.open(DATA_CACHE_NAME).then(cache => {
-              return fetch(evt.request)
-                  .then(response => {
-                      // If the response was good, clone it and store it in the cache.
-                      if (response.status === 200) {
-                          cache.put(evt.request.url, response.clone());
-                      }
-                      return response;
-                  })
-                  .catch(err => {
-                      // Network request failed, try to get it from the cache.
-                      return cache.match(evt.request);
-                  });
-          }).catch(err => console.log(err))
-      );
-      return;
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
   }
   // if the request is not for the API, serve static assets using "offline-first" approach.
   // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
   evt.respondWith(
-      fetch(evt.request).catch(function() {
-          return caches.match(evt.request).then(function(response) {
-              if (response) {
-                  return response;
-              } else if (evt.request.headers.get("accept").includes("text/html")) {
-                  return caches.match("/");
-              }
-          });
-      })
-  )
+    fetch(evt.request).catch(function () {
+      return caches.match(evt.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
 });
